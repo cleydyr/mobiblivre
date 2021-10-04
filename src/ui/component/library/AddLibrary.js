214183bb1175b23e1b7bbed15b7456073a30833d
Cleydyr Albuquerque <cleydyr@gmail.com>
2021-10-04 20:26:26 +0200

    add library form

---
+++ src/ui/component/library/AddLibrary.js
@@ -0,0 +1,79 @@
+import React, { useState } from "react";
+import { StyleSheet, View } from "react-native";
+import { Button, Switch, Text, TextInput } from "react-native-paper";
+
+export default () => {
+  const [name, setName] = useState('');
+  const [url, setURL] = useState('');
+  const [isAuthenticated, setAuthenticated] = useState(false);
+  const [userName, setUserName] = useState('');
+  const [password, setPassword] = useState('');
+  const [isShowPassword, setShowPassword] = useState(false);
+
+  const toggleAuthenticated = () => setAuthenticated(!isAuthenticated);
+  const toggleShowPassword = () => setShowPassword(!isShowPassword);
+
+  return (
+    <View
+      style={{
+        flex: 1,
+      }}>
+
+      <TextInput
+        label="Nome"
+        value={name}
+        onChangeText={setName}
+        style={styles.formField}
+      />
+
+      <TextInput
+        label="URL"
+        value={url}
+        onChangeText={setURL}
+        placeholder="exemplo: https://minhabiblioteca.biblivre.cloud"
+        style={styles.formField}
+      />
+
+      <View style={{
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+      }}>
+        <Text style={[
+          styles.formField,
+          {
+            fontWeight: "600",
+            fontSize: 20,
+          }]}>Necessita autenticação</Text>
+        <Switch value={isAuthenticated} onValueChange={toggleAuthenticated} />
+      </View>
+
+      {
+        isAuthenticated
+        && <>
+          <TextInput
+            label="Usuário"
+            value={userName}
+            onChangeText={setUserName}
+            style={styles.formField}
+          />
+          <TextInput
+            label="Senha"
+            value={password}
+            secureTextEntry={!isShowPassword}
+            onChangeText={setPassword}
+            right={<TextInput.Icon name={`eye${isShowPassword ? "-off" : ""}`} onPress={toggleShowPassword} />}
+            style={styles.formField}
+          />
+        </>
+      }
+
+      <Button style={styles.formField} mode="contained" >Salvar</Button>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  formField: {
+    margin: 16,
+  }
+});
